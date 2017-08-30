18c680e0d40239ed50b6c237e5d5be662466091a
TechnoWormie <technowormie@gmail.com>
2017-08-30 11:26:48 +0530

    My First Commit

---
+++ script.js
@@ -0,0 +1,95 @@
+
+
+
+/*Social Networks*/
+
+$('.social-networks').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 5,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+/*Live Scores*/
+
+$('.live-scores').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 5,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+/* Technology */
+
+$('.technology').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 5,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
